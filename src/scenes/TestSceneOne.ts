0c4bd0fe188e22ff50cbcbde1e68bab0c49acd83
MrKiplin <tjworks@hotmail.co.uk>
2020-07-05 00:39:44 +0100

    Update key strings for enums

--- src/scenes/TestSceneOne.ts
+++ src/scenes/TestSceneOne.ts
@@ -1,5 +1,23 @@
 import Phaser from "phaser";
 
+enum ImageKeys {
+  SKY = "sky",
+  GROUND = "ground",
+  STAR = "star",
+  BOMB = "bomb",
+  DUDE = "dude",
+}
+
+enum AudioKeys {
+  COLLECT = "collect",
+}
+
+enum AnimKeys {
+  LEFT = "left",
+  RIGHT = "right",
+  TURN = "turn",
+}
+
 export default class TestSceneOne extends Phaser.Scene {
   private player!: Phaser.Physics.Arcade.Sprite;
   private stars!: Phaser.Physics.Arcade.Group;
@@ -17,49 +35,56 @@ export default class TestSceneOne extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("sky", "assets/sky.png");
-    this.load.image("ground", "assets/platform.png");
-    this.load.image("star", "assets/star.png");
-    this.load.image("bomb", "assets/bomb.png");
-    this.load.spritesheet("dude", "assets/dude.png", {
+    this.load.image(ImageKeys.SKY, "assets/sky.png");
+    this.load.image(ImageKeys.GROUND, "assets/platform.png");
+    this.load.image(ImageKeys.STAR, "assets/star.png");
+    this.load.image(ImageKeys.BOMB, "assets/bomb.png");
+    this.load.spritesheet(ImageKeys.DUDE, "assets/dude.png", {
       frameWidth: 32,
       frameHeight: 48,
     });
-    this.load.audio("collect", "assets/sounds/completetask_0.mp3");
+
+    this.load.audio(AudioKeys.COLLECT, "assets/sounds/completetask_0.mp3");
   }
 
   create() {
-    this.add.image(0, 0, "sky").setOrigin(0, 0);
+    this.add.image(0, 0, ImageKeys.SKY).setOrigin(0, 0);
 
     const platforms: Phaser.Physics.Arcade.StaticGroup = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    platforms.create(400, 568, ImageKeys.GROUND).setScale(2).refreshBody();
 
-    platforms.create(600, 400, "ground");
-    platforms.create(50, 250, "ground");
-    platforms.create(750, 220, "ground");
+    platforms.create(600, 400, ImageKeys.GROUND);
+    platforms.create(50, 250, ImageKeys.GROUND);
+    platforms.create(750, 220, ImageKeys.GROUND);
 
-    this.player = this.physics.add.sprite(100, 450, "dude");
+    this.player = this.physics.add.sprite(100, 450, ImageKeys.DUDE);
 
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
 
     this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      key: AnimKeys.LEFT,
+      frames: this.anims.generateFrameNumbers(ImageKeys.DUDE, {
+        start: 0,
+        end: 3,
+      }),
       frameRate: 10,
       repeat: -1,
     });
 
     this.anims.create({
-      key: "turn",
-      frames: [{ key: "dude", frame: 4 }],
+      key: AnimKeys.TURN,
+      frames: [{ key: ImageKeys.DUDE, frame: 4 }],
       frameRate: 20,
     });
 
     this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      key: AnimKeys.RIGHT,
+      frames: this.anims.generateFrameNumbers(ImageKeys.DUDE, {
+        start: 5,
+        end: 8,
+      }),
       frameRate: 10,
       repeat: -1,
     });
@@ -67,7 +92,7 @@ export default class TestSceneOne extends Phaser.Scene {
     this.physics.add.collider(this.player, platforms);
 
     this.stars = this.physics.add.group({
-      key: "star",
+      key: ImageKeys.STAR,
       repeat: 11,
       setXY: { x: 12, y: 0, stepX: 70 },
     });
@@ -102,7 +127,7 @@ export default class TestSceneOne extends Phaser.Scene {
       this
     );
 
-    this.collectStarSound = this.sound.add("collect");
+    this.collectStarSound = this.sound.add(AudioKeys.COLLECT);
   }
 
   update() {
@@ -111,15 +136,15 @@ export default class TestSceneOne extends Phaser.Scene {
     if (cursors.left?.isDown) {
       this.player.setVelocityX(-160);
 
-      this.player.anims.play("left", true);
+      this.player.anims.play(AnimKeys.LEFT, true);
     } else if (cursors.right?.isDown) {
       this.player.setVelocityX(160);
 
-      this.player.anims.play("right", true);
+      this.player.anims.play(AnimKeys.RIGHT, true);
     } else {
       this.player.setVelocityX(0);
 
-      this.player.anims.play("turn");
+      this.player.anims.play(AnimKeys.TURN);
     }
 
     if (cursors.up?.isDown && this.player.body.touching.down) {
@@ -151,7 +176,7 @@ export default class TestSceneOne extends Phaser.Scene {
             ? Phaser.Math.Between(400, 800)
             : Phaser.Math.Between(0, 400);
 
-        var bomb = this.bombs.create(x, 16, "bomb");
+        var bomb = this.bombs.create(x, 16, ImageKeys.BOMB);
         bomb.setBounce(1);
         bomb.setCollideWorldBounds(true);
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
@@ -165,7 +190,7 @@ export default class TestSceneOne extends Phaser.Scene {
   ) {
     this.physics.pause();
     this.player.setTint(0xff0000);
-    this.player.anims.play("turn");
+    this.player.anims.play(AnimKeys.TURN);
 
     this.gameOver = true;
   }
